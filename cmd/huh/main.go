6f6a890e4978ea6982dd2e2ec4a7c6367c86acd7
Will Gorman <will.gorman@joyent.com>
2019-09-01 22:01:51 -0500

    It's a start

---
+++ cmd/huh/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/api/switch", SwitchStatusHandler)
+	r.HandleFunc("/api/switch/on", SwitchHandler(true))
+	r.HandleFunc("/api/switch/off", SwitchHandler(false))
+	r.HandleFunc("/api/brightness", BrightnessHandler)
+	r.HandleFunc("/api/brightness/{value}", SetBrightnessHandler)
+	r.HandleFunc("/api/color", ColorHandler)
+	r.HandleFunc("/api/color/{value}", SetColorHandler)
+	http.Handle("/", r)
+
+	srv := &http.Server{
+		Addr: "0.0.0.0:8080",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
+
+}
+
+func SwitchStatusHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	log.Infof("Getting switch status")
+	io.WriteString(w, "0")
+}
+
+func SwitchHandler(on bool) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	}
+}
+
+func BrightnessHandler(w http.ResponseWriter, r *http.Request) {
+	log.Infof("Getting brightness %s", 100)
+	w.WriteHeader(200)
+	io.WriteString(w, "100")
+}
+
+func SetBrightnessHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	brightness := vars["value"]
+	log.Infof("Setting brightness to %s", brightness)
+	w.WriteHeader(200)
+}
+
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	log.Infof("Getting color")
+	w.WriteHeader(200)
+	io.WriteString(w, "000000")
+}
+
+func SetColorHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	color := vars["value"]
+	log.Infof("Setting color", color)
+	w.WriteHeader(200)
+}
